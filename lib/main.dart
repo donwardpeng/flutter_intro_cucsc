12637a6faf48368e1781430f6dae5708f75fc737
donwardpeng <don.ward.peng@gmail.com>
2019-07-23 16:46:30 -0400

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+// Declare a set list of images to cycle through as a final
+  final List<Image> _itemImages = [
+    Image.asset('assets/Google.png'),
+    Image.asset('assets/dtw.png'),
+    Image.asset('assets/GDGDetroit.png')
+  ];
+
+  int _index = 0;
+  List<Image> _listOfImagesForScreen = [];
+
+  void onButtonPressed() {
+    setState(() {
+      _index++;
+      if (_index >= _itemImages.length) {
+        _index = 0;
+      }
+      _listOfImagesForScreen.add(_itemImages[_index]);
+      // print('Item Name = ' + _listOfImagesForScreen.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,30 +39,33 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               title: Text('Detroit Tech Watch'),
             ),
-           body: Stack(
+            body: Stack(
               children: <Widget>[
                 ListView(
-                  children: <Widget>[
-                    Card(
-                      margin: EdgeInsets.all(8),
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset('assets/dtw.png'),
-                          Padding(
-                            padding: const EdgeInsets.all(8),
-                          ),
-                          Text('Detroit Tech Watch')
-                        ],
-                      ),
-                    ),
-                  ],
+                  children: _listOfImagesForScreen
+                      .map((element) => Card(
+                            margin: EdgeInsets.all(8),
+                            child: Column(
+                              children: <Widget>[
+                                element,
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                ),
+                              ],
+                            ),
+                          ))
+                      .toList(),
                 ),
-                FloatingActionButton(
-                  child: const Icon(Icons.add),
-                  onPressed: () {},
-                )
+                Positioned(
+                    bottom: 16.0,
+                    right: 16.0,
+                    child: FloatingActionButton(
+                      child: const Icon(Icons.add),
+                      onPressed: () {
+                        onButtonPressed();
+                      },
+                    ))
               ],
             )));
   }
 }
-
